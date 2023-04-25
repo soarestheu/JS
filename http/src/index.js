642b90b7ec9ce7b5a82c3e1ab8e6623eaf22972b
Matheus Soares <soarestheu@MacBook-Pro-de-Matheus.local>
2023-04-25 00:25:14 -0300

    Primeiro commit

---
+++ http/src/index.js
@@ -0,0 +1,40 @@
+const http = require('http');
+const { URL } = require('url');
+
+const routes = require('./routes');
+const path = require('path');
+
+const server = http.createServer((request, response) => {
+    const parsedUrl = new URL(`http://localhost:3000${request.url}`);
+
+    let { pathname } = parsedUrl
+    let id;
+    const splitEndpoit = pathname.split('/').filter((Boolean))
+
+    if (splitEndpoit.length > 1) {
+        pathname = `/${splitEndpoit[0]}/:id`;
+        id = splitEndpoit[1];
+    }
+
+    const route = routes.find((routeObj) => (
+        routeObj.endpoint === pathname && routeObj.method === request.method
+    ))
+
+
+
+    if (route) {
+        request.query = Object.fromEntries(parsedUrl.searchParams);
+        request.params = { id }
+        response.send = (statusCode, Body) => {
+            response.writeHead(statusCode, { 'Content-type': 'application/json' })
+            response.end(JSON.stringify(Body));
+        }
+        route.handler(request, response);
+    } else {
+        response.writeHead(200, { 'Content-type': 'text/html' });
+        response.end("<h1>TESTE DE TESTE</h1>")
+    }
+
+})
+
+server.listen(3000, () => console.log(" SERVIDOR ONLINE http://localhost:3000"))
---
+++ http/src/mocks/index.js
@@ -0,0 +1,13 @@
+module.exports = [{
+        id: 1,
+        nome: "Matheus"
+    },
+    {
+        id: 2,
+        nome: "Tatiane"
+    },
+    {
+        id: 3,
+        nome: "Ju"
+    },
+]
